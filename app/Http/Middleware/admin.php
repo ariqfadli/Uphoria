d96736ff83dc9c4af445f0538302395aca7a3f2f
ariqfadli <attariqfadli@students.unnes.ac.id>
2023-06-11 16:16:49 +0700

    tambah-tambah

--- app/Http/Middleware/admin.php
+++ app/Http/Middleware/admin.php
@@ -13,7 +13,7 @@ class admin
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         if(!auth()-> check || !auth()->user()->is_admin){
             abort(403);
